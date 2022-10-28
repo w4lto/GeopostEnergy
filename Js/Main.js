9bfc136558c88349acf44594b7f28777d6861c9d
Joao Pedro Albergaria de Castro <jpbrasil373@gmail.com>
2022-10-28 02:30:54 -0300

    Comit 3

--- Js/Main.js
+++ Js/Main.js
@@ -1,13 +1,45 @@
+const api_url = "https://estagio.geopostenergy.com/WorldCup/GetAllTeams"; 
+// Header : "git-user : w4lto";
+// Response : JSON; 
+
 var request = new Request('https://estagio.geopostenergy.com/WorldCup/GetAllTeams', {
     method: 'GET',
     headers: new Headers({'git-user' : 'w4lto'})
 });
 
+// let x = undefined
+
+// var obj
+
+// fetch(request)
+//     .then((response) => response.json())
+//     .then((responseJson) => { 
+//         obj = responseJson;
+//     })
+//     .then(() =>{
+//         console.log(obj);
+//     })
+//     .catch((error) => {
+//         console.error(error);
+//     });
+
+
+var obj
+
 fetch(request)
     .then((response) => response.json())
-    .then((responseJson) => {
-        console.log(requestJson); 
+    .then(data => { 
+        obj = data;
+    })
+    .then(() => {
+        console.log(obj);
     })
     .catch((error) => {
         console.error(error);
-    });
+    })
+
+
+// times = fetch(request).then(); 
+
+// console.log(x, "AAAAAA"); 
+    
