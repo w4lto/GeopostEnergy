cfcc36c9e8a57ddbc49d22f7a0aaec23a56f4ee6
Joao Pedro Albergaria de Castro <jpbrasil373@gmail.com>
2022-10-27 23:57:19 -0300

    Comit1

---
+++ Js/Main.js
@@ -0,0 +1,15 @@
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+fetch(url)
+    .them(function(data) {
+
+    })
+    .catch(function(error) {
+
+    });
